ec41d7aad6b195b0c6e6406bd0af6f5d2512c18b
tacharya2 <60335154+tacharya2@users.noreply.github.com>
2021-01-28 16:56:36 -0500

    init

---
+++ test.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercise 1: Functions</title>
+  <style>
+    #navigation {
+      margin-left: auto;
+      margin-right: auto;
+      color: greenyellow;
+      font-size: 1.5rem;
+      padding: 1rem;
+    }
+  </style>
+</head>
+
+<body>
+  <h5>Exercise 1</h5>
+  <?php
+  function hello_world()
+  {
+    echo "Hello World!";
+  }
+  hello_world();
+  ?>
+  <br>
+  <br><br>
+  <div id ="navigation">
+    <a href="Exercise2.php">Exercise 2</a>
+    <br>
+    <a href="Exercise3.php">Exercise 3</a>
+    <br>
+    <a href="Exercise4.php">Exercise 4</a>
+    <br>
+    <a href="Exercise5.php">Exercise 5</a>
+    <br>
+    <a href="Exercise6.php">Exercise 6</a>
+  </div>
+</body>
+
+</html>
